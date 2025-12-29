a3a99a3fd11fa680825dacebc844b58aae1afca0
DESKTOP-T1C16EV\4s-family <artistsh07@gmail.com>
2025-12-30 03:03:25 +0900

    달력 호출 버튼 제작

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 #학업 계획&진행 여부 기록 프로그램 만들기
+
+#함수 호출
 import tkinter
 
 tk = tkinter
 
+#창 설정
 win = tk.Tk()
 win.title('엘든컴')
 win.geometry("600x840+0+0")
 win.resizable(1,1)  #크기 조절 가능
 
-logo = tk.Label(win, text='스케줄 매니저\n엘든컴', anchor='center', fg='black', font=('SimSun-ExtB',12,"bold"))
+#로고
+logo = tk.Label(win, text='스케줄 매니저\n엘든컴', anchor='center', fg='black', font=('SimSun-ExtB',30,"bold"))
 logo.place(relx=0.5, rely=0, anchor='n')
+
+
+#달력 호출 버튼
+def CalBtn():
+    pass
+
+calBtn = tk.Button(win, text='달력', command=CalBtn, font=('SimSun-ExtB',15,"bold"))
+calBtn.place(relx=0.05, rely=0.2, anchor='w', relwidth=0.2, relheight=0.1)
 
 win.mainloop()
