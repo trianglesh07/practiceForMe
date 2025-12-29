8a8c0e3f5f07052666cf03eb2d3a91b3702cfc12
DESKTOP-T1C16EV\4s-family <artistsh07@gmail.com>
2025-12-30 02:51:38 +0900

    창 크기 조절 및 이름 설정 및 로고 제작

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import tkinter
 tk = tkinter
 
 win = tk.Tk()
-win.title('가명')
+win.title('엘든컴')
+win.geometry("600x840+0+0")
+win.resizable(1,1)  #크기 조절 가능
+
+logo = tk.Label(win, text='스케줄 매니저\n엘든컴', anchor='center', fg='black', font=('SimSun-ExtB',12,"bold"))
+logo.place(relx=0.5, rely=0, anchor='n')
 
 win.mainloop()
