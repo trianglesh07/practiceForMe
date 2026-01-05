89b413e782e3be896845fc404f1d355685427b9c
DESKTOP-T1C16EV\4s-family <artistsh07@gmail.com>
2026-01-06 01:52:40 +0900

    달력 기본 구조 제작

--- main.py
+++ main.py
@@ -17,24 +17,35 @@ logo.place(relx=0.5, rely=0, anchor='n')
 
 
 #달력 호출 버튼
+#달력 만들기
+cal = tk.Frame(win, width=560, height=500, bg="#D3D3D3")
+for a in range(7):
+    for b in range(5):
+        date=(a+7*b+1)
+        if date <= 31:
+            cell = tk.Label(cal, text=str(date), borderwidth=1)
+            cell.place(relx=0.14*a, rely=0.2*b, anchor="nw", relwidth=(1/6), relheight=0.2)
+        else:
+            cell = tk.Label(cal, text="Elden Ring\nis badass", borderwidth=1)
+            cell.place(relx=0.14 * a, rely=0.2 * b, anchor="nw", relwidth=(1 / 6), relheight=0.2)
 def CalBtn():
-    pass
+    cal.place(relx=0.5, rely=0.9, anchor="s", relwidth=0.9, relheight=0.65)
 
-calBtn = tk.Button(win, text='달력', command=CalBtn, font=('SimSun-ExtB',15,"bold"))
+calBtn = tk.Button(win, text='달력', command=CalBtn, font=('SimSun-ExtB',15,"bold"), relief="groove")
 calBtn.place(relx=0.05, rely=0.2, anchor='w', relwidth=0.2, relheight=0.1)
 
 #할일 목록 호출 버튼
 def DoList():
     pass
 
-dlBtn = tk.Button(win, text='해야 할 일', command=DoList, font=('SimSun-ExtB',15,"bold"))
+dlBtn = tk.Button(win, text='해야 할 일', command=DoList, font=('SimSun-ExtB',15,"bold"), relief="groove")
 dlBtn.place(relx=0.3, rely=0.2, anchor='w', relwidth=0.2, relheight=0.1)
 
 #타이머 호출 버튼
 def Timer():
     pass
 
-tmBtn = tk.Button(win, text='타이머', command=Timer, font=('SimSun-ExtB',15,"bold"))
+tmBtn = tk.Button(win, text='타이머', command=Timer, font=('SimSun-ExtB',15,"bold"), relief="groove")
 tmBtn.place(relx=0.55, rely=0.2, anchor='w', relwidth=0.2, relheight=0.1)
 
 win.mainloop()
